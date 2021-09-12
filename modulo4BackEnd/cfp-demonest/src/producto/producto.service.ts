14730fb0d403c61f4dadc855cab65836650385ec
Defelippekevin <kevin.defelippe@gmail.com>
2021-09-12 17:14:20 -0300

    subo el codigo hecho en la clase del viernes

--- modulo4BackEnd/cfp-demonest/src/producto/producto.service.ts
+++ modulo4BackEnd/cfp-demonest/src/producto/producto.service.ts
@@ -1,40 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import Producto from './Producto';
 
 @Injectable()
 export class ProductoService {
-    public getProductos(): any{
-        let arrayCompras=[
-            {
-                "producto": "Pera",
-                "precio": 50,
-                "descripcion": "muy rica pera"
-            },
-            {
-                "producto": "mandarina",
-                "precio": 70,
-                "descripcion": "medias agrias"
-            },
-            {
-                "producto": "naranja",
-                "precio": 60,
-                "descripcion": "bien jugosas"
-            },
-            {
-                "producto": "bananas",
-                "precio": 40,
-                "descripcion": "riquisimas"
-            },
-            {
-                "producto": "kiwi",
-                "precio": 80,
-                "descripcion": "ideal para una ensalada de fruta"
-            },
-            {
-                "producto": "sandia",
-                "precio": 190,
-                "descripcion": "re contra dulces0"
-            }
-        ]; 
-        return arrayCompras;
+    private listaProductos:Producto[]=[];
+
+    public constructor(){
+        this.loadProductos()
+    }
+    
+    public getProductos(): Producto[]{
+        return this.listaProductos;
+    }
+
+    public getProducto(id:number):Producto{
+        let producto = null;
+// Más adelante agregar manejo de status code
+        for (let i=0; i<this.listaProductos.length; i++) {
+            if (this.listaProductos[i].getIdProducto()==id) {
+            producto = this.listaProductos[i];
+        }
+    }
+        return producto;
+   }
+
+    private loadProductos(): void {
+        let archivo = fs.readFileSync('productos.csv', 'utf8');
+        const elementos = archivo
+        .split('\n')
+        .map(p => p.replace('\r', ''))
+        .map(p => p.split(','));
+        this.listaProductos = [];
+        for (let i = 0; i < elementos.length; i++) {
+        let producto = new Producto(parseInt(elementos[i][0]),
+        elementos[i][1], parseFloat(elementos[i][2]));
+        this.listaProductos.push(producto);
+        }
     }
 }
